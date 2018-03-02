8ba840038d9a698303b33a580fa4bd293c76240c
LauJames <LauJames_work@163.com>
2018-03-02 10:37:15 +0800

    + add runBiLSTM.py

---
+++ runBiLSTM.py
@@ -0,0 +1,13 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : Tagging 
+@File     : runBiLSTM.py
+@Time     : 2018/3/2 10:35
+@Software : PyCharm
+@Copyright: "Copyright (c) 2018 Lau James. All Rights Reserved"
+"""
